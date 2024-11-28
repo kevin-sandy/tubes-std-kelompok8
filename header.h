3e1d64b82b29b4b2b3661560a8e27639e6f269d6
kevin-sandy <kevinmanurung013@gmail.com>
2024-11-28 17:12:26 +0700

    Update header.h

--- header.h
+++ header.h
@@ -5,4 +5,25 @@
 
 using namespace std;
 
+//array undo/redo
+const int MAXSIZE = 1000;
+typedef int index;
+
+struct stack {
+  index top;
+  adr info[MAXSIZE]
+};
+
+//DLL text editor
+typedef struct elmList *adr;
+typedef string infotype;
+
+struct elmList {
+  infotype info;
+  adr prev, next;
+};
+struct list {
+  adr first, last;
+};
+
 #endif // HEADER_H_INCLUDED
