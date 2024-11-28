bde44d2901147e437c5ee40faa6ad5f70c8b7dec
kevin-sandy <kevinmanurung013@gmail.com>
2024-11-28 18:00:29 +0700

    delete last uploaded
    selesain delete last 3 kondisi

--- source.cpp
+++ source.cpp
@@ -1,2 +1,19 @@
 #include "header.h"
 
+void deleteLast(list &L, adr &P) {
+  P = L.last;
+  if (L.first == Nil) { //list kosong
+    cout << "List kosong." << endl;
+  } else if (L.first == L.last) { //list memiliki satu elemen
+    L.first = Nil;
+    L.last = Nil;
+  } else { //list memiliki lebih dari satu elemen
+    L.last = P->prev;
+    L.last->next = Nil;
+    P->prev = Nil;
+  }
+}
+
+void deleteAfter(list &L, adr Pred, adr &P) {
+
+}
