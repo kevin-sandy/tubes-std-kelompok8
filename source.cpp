980d3d395218389bafc5bb120dd2824fbea6fc41
kevin-sandy <kevinmanurung013@gmail.com>
2024-11-29 08:14:19 +0700

    update kevin

--- source.cpp
+++ source.cpp
@@ -1,31 +1,52 @@
 #include "header.h"
 
-void deleteLast(list &L, adr &P) { //delete elemen L.first
-  P = L.last;
-  if (L.first == Nil) { //list kosong
-    cout << "List kosong." << endl;
-  } else if (L.first == L.last) { //list memiliki satu elemen
-    L.first = Nil;
-    L.last = Nil;
-  } else { //list memiliki lebih dari satu elemen
-    L.last = P->prev;
-    L.last->next = Nil;
-    P->prev = Nil;
+void pesanDelete(bool stat) { // tampilkan ke layar apakah terjadi delete atau tidak
+  if (stat) { // terjadi delete
+    cout << "Elemen berhasil dihapus." << endl;
+  } else { // tidak terjadi delete
+    cout << "Elemen tidak ada." << endl;
   }
 }
 
-void deleteAfter(list &L, adr Pred, adr &P) { //delete elemen setelah Pred
-  P = Pred->next
-  if (P->next == Nil) { //tidak ada elemen setelah P
+void deleteLast(list &L, adr &P) { // delete elemen L.first
+  bool statusDelete; // status apakah delete atau tidak
+  P = L.last;
+  if (L.first == Nil) { //list kosong
+    statusDelete = false;
+  } else if (L.first == L.last) { // list memiliki satu elemen
+    L.first = Nil;
+    L.last = Nil;
+    statusDelete = true;
+  } else { // list memiliki lebih dari satu elemen
+    L.last = P->prev;
+    L.last->next = Nil;
+    P->prev = Nil;
+    statusDelete = true;
+  }
+
+  // pesan apakah terjadi delete atau tidak
+  pesanDelete(statusDelete);
+}
+
+void deleteAfter(list &L, adr Pred, adr &P) { // delete elemen setelah Pred
+  bool statusDelete; // status apakah delete atau tidak
+  P = Pred->next;
+  if (P == Nil) {
+    statusDelete = false;
+  } else if (P->next == Nil) { // tidak ada elemen setelah P
     L.last = Pred;
     P->prev = Nil;
     Pred->next = Nil;
-  } else if (P->next != Nil) { //ada elemen setelah P
+    statusDelete = true;
+  } else if (P->next != Nil) { // ada elemen setelah P
     Pred->next = P->next;
     P->next->prev = Pred;
     P->next = Nil;
     P->prev = Nil;
-  } else if (P == Nil) {
-    cout << "Tidak ada elemen setelah cursor" << endl;
+    statusDelete = true;
   }
+
+  // pesan apakah terjadi delete atau tidak
+  pesanDelete(statusDelete);
 }
+
