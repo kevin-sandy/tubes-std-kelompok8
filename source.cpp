63ede6ba80cbf933b9e733c9c7a9eb31519ed71b
kevin-sandy <kevinmanurung013@gmail.com>
2024-11-29 08:03:25 +0700

    delete after updated

--- source.cpp
+++ source.cpp
@@ -1,6 +1,6 @@
 #include "header.h"
 
-void deleteLast(list &L, adr &P) {
+void deleteLast(list &L, adr &P) { //delete elemen L.first
   P = L.last;
   if (L.first == Nil) { //list kosong
     cout << "List kosong." << endl;
@@ -14,6 +14,18 @@ void deleteLast(list &L, adr &P) {
   }
 }
 
-void deleteAfter(list &L, adr Pred, adr &P) {
-
+void deleteAfter(list &L, adr Pred, adr &P) { //delete elemen setelah Pred
+  P = Pred->next
+  if (P->next == Nil) { //tidak ada elemen setelah P
+    L.last = Pred;
+    P->prev = Nil;
+    Pred->next = Nil;
+  } else if (P->next != Nil) { //ada elemen setelah P
+    Pred->next = P->next;
+    P->next->prev = Pred;
+    P->next = Nil;
+    P->prev = Nil;
+  } else if (P == Nil) {
+    cout << "Tidak ada elemen setelah cursor" << endl;
+  }
 }
