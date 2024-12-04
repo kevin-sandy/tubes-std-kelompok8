13503d6b948e922409b300b58b5eff309d77e8b2
kevin-sandy <kevinmanurung013@gmail.com>
2024-12-04 21:18:02 +0700

    Update header.h kevin -undoredo

--- header.h
+++ header.h
@@ -1,19 +1,24 @@
 #ifndef HEADER_H_INCLUDED
 #define HEADER_H_INCLUDED
 #define Nil NULL
+
 #include <iostream>
 
 using namespace std;
 
 // Stack
 const int MAXSIZE = 10000;
+struct infoUndoRedo {
+    string aksi;
+    char karakter;
+};
 struct infotypeStack {
     string action;
-    infotypeList info;
+    infoUndoRedo info;
 };
 typedef int index;
 struct Stack {
-    infotypeStack info[MAXSIZE];
+    infoUndoRedo info[MAXSIZE];
     index top;
 };
 
@@ -23,33 +28,64 @@ bool isEmpty(Stack S);
 
 bool isFull(Stack S);
 
-void push(Stack &S, infotypeStack i);
+void push(Stack &S, infoUndoRedo i);
 
-void pop(Stack &S, infotypeStack i);
+void pop(Stack &S, infoUndoRedo i);
 
-infotypeStack peek(Stack S);
+infoUndoRedo peek(Stack S);
 
 // DLL
 typedef struct elmList *adr;
-typedef char infotypeList;
+typedef char infotype;
 struct elmList {
-    infotypeList info;
+    infotype info;
     adr prev, next;
 };
 struct List {
     adr first, last;
 };
 
-List createList();
+void createList(List &L, adr &C);
 
-adr createElmList(infotype x);
+adr createElmList(infotype value);
 
-void insertAfterCursor(List &L, adr cursor, adr &P);
+void insertFirst(List &L, adr &P);
 
-void deleteLast(List &L, adr P);
+void insertAfter(List &L, adr &P, adr prec);
 
-void deleteAfter(List &L, adr P, adr Prec);
+void insertLast(List &L, adr &P);
+
+void deleteFirst(List &L, adr &P);
+
+void deleteAfter(List &L, adr &P, adr Prec);
+
+void deleteBefore(List &L, adr &P, adr Pred);
+
+void deleteLast(List &L, adr &P);
 
 void printText(List L);
+
+void printTextWithCursor(List L, adr C);
+
+void addText(List &L, adr &C);
+
+adr setCursor(List &L);
+
+void moveCursorLeft(List &L, adr &C);
+
+void moveCursorRight(List &L, adr &C);
+
+void Backspace(List &L, adr &C);
+
+void Delete(List &L, adr &C);
+
+// Menu
+void input_text(List &L, adr &C);
+
+void print_text(List L);
+
+void pindahkan_kursor(List L, adr &C);
+
+void hapus_teks(List &L, adr &C);
 
 #endif // HEADER_H_INCLUDED
